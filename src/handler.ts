83aa0cdec064f905945ab5b3e0c9b2716a5b8e3e
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-04 17:22:45 +0000

    Add class and method for reading files

---
+++ src/handler.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs'
+
+export class FileHandler {
+  read(): object[] {
+    try {
+      const data = readFileSync('./data/airports.json', 'utf-8')
+      return JSON.parse(data)
+    } catch (error) {
+      console.error(`Error reading JSON: ${error}`)
+    }
+    throw new Error('Could not read data file')
+  }
+}
+
+export class ArgumentHandler {}
