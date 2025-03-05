b815688ab049205c2efabb843b8cabcbe7e200d4
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-05 16:32:55 +0000

    Complete main logic for CLI tool

--- src/cli.ts
+++ src/cli.ts
@@ -7,25 +7,64 @@ import {
 import { FileHandler, ArgumentHandler } from './handler.js'
 import { FlightService, WeatherService } from './service.js'
 import { input, select } from '@inquirer/prompts'
-import { match } from 'assert'
 
 export class AeroplaneSearcherCLI {
+  private readonly loadedAirportData: Airport[] = FileHandler.read()
   /*
-   * Gets name of airport from user
-   * From command line argument, or user input in cli
-   * Find all airports that match the name
-   * if theres only one match, select automatically
    * Find all the flights leaving that airport
    * Extract the location of all of the destinations of the airports
    * Combine that data with the weather at each of the destinations of the flights
    * Export this data to a HTML file
    */
 
+  async getAndFormatData(
+    flightsApiKey: string,
+    weatherApiKey: string,
+    iata: string,
+  ): Promise<TransformedFlightData[]> {
+    const flightService = new FlightService(flightsApiKey)
+    const weatherService = new WeatherService(weatherApiKey)
+
+    const formattedData: TransformedFlightData[] = []
+
+    const allFlights: RawFlightData[] =
+      await flightService.fetchFlightData(iata)
+
+    for (const flight of allFlights) {
+      const weatherData: RawWeatherData =
+        await weatherService.fetchWeatherByIata(flight.arr_iata)
+
+      formattedData.push({
+        flightNumber: flight.flight_number,
+        departureTime: flight.dep_time,
+        destinationAirport: this.getAirportName(flight.arr_iata) || 'idk',
+        destinationTemperature: weatherData.temp_c,
+        condition: weatherData.condition,
+        delay: flight.arr_delayed || undefined,
+      })
+    }
+
+    return formattedData
+  }
+
   async start() {
+    const flightApiKey = process.env.FLIGHT_API_KEY
+    const weatherApiKey = process.env.WEATHER_API_KEY
+
+    if (flightApiKey === undefined || weatherApiKey === undefined) {
+      throw new Error('API keys not found...')
+    }
+
     this.printGreeting()
     const foundMatches = this.getMatches(await this.getUserInput())
-    const matchingAirport = this.getChoiceFromMatches(foundMatches)
-    console.log(await matchingAirport)
+    const matchingAirport = await this.getChoiceFromMatches(foundMatches)
+    console.log(`Retrieving flight data for ${matchingAirport.name}`)
+    const data = await this.getAndFormatData(
+      flightApiKey,
+      weatherApiKey,
+      matchingAirport.iata,
+    )
+    console.log(data)
   }
 
   printGreeting(): void {
@@ -47,15 +86,17 @@ export class AeroplaneSearcherCLI {
   }
 
   getMatches(input: string): Airport[] {
-    const airports: Airport[] = FileHandler.read()
-
-    const matches = airports.filter((airport: Airport) => {
+    const matches = this.loadedAirportData.filter((airport: Airport) => {
       if (airport.name !== null) {
         return airport.name.toLowerCase().includes(input)
       }
     })
 
     return matches
+  }
+
+  private getAirportName(iata: string): string | undefined {
+    return this.loadedAirportData.find((airport) => airport.iata === iata)?.name
   }
 
   private createAirportSelection(airports: Airport[]) {
@@ -65,17 +106,17 @@ export class AeroplaneSearcherCLI {
     }))
   }
 
-  async getChoiceFromMatches(matches: Airport[]): Promise<string> {
+  async getChoiceFromMatches(matches: Airport[]): Promise<Airport> {
     if (matches.length === 0) {
       throw new Error('Could not find any airports matching that name.')
     } else if (matches.length === 1) {
-      return matches[0].iata
+      return matches[0]
     } else {
       const selectedAirport = await select({
         message: 'Multiple airports found, please select one:',
         choices: this.createAirportSelection(matches),
       })
-      return selectedAirport.iata
+      return selectedAirport
     }
   }
 }
