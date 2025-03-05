34acc7a7b38aa6dc8e90cab25c021ec1809a4e32
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-05 13:09:10 +0000

    Add method for fetching weather data from api

--- src/service.ts
+++ src/service.ts
@@ -2,8 +2,9 @@ import {
   RawFlightData,
   RawWeatherData,
   TransformedFlightData,
-} from './interface'
+} from './interface.js'
 import 'dotenv/config'
+import axios from 'axios'
 
 export class FlightService {
   private flights: TransformedFlightData[] = []
@@ -27,5 +28,23 @@ export class WeatherService {
       throw new Error('Could not retrieve Weather API key from .env')
     }
     this.apiKey = apiKey
+  }
+
+  async fetchWeatherByIata(iata: string): Promise<RawWeatherData> {
+    try {
+      const url = `${this.baseURL}${this.apiKey}&q=iata:${iata}%aqi=no`
+      console.log(url)
+      const response = await axios.get(url)
+      const data = response.data.current
+
+      return { temp_c: data.temp_c, condition: data.condition.text }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 404) {
+        console.error(`Could not get weather at iata: ${iata}`)
+      } else {
+        console.error(`Error fetching data: ${error}`)
+      }
+      throw new Error(`Error fetching weather data: ${error}`)
+    }
   }
 }
