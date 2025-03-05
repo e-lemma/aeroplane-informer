6973bb6c0bee70473baf64307d8917ff3de56501
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-05 15:43:34 +0000

    Adjust Airport interface to match data from JSON

--- src/interface.ts
+++ src/interface.ts
@@ -20,6 +20,13 @@ export interface TransformedFlightData {
 }
 
 export interface Airport {
+  iata: string
+  lon: string
+  iso: string
+  status: number
   name: string
-  iata: number
+  continent: string
+  type: string
+  lat: string
+  size: string
 }
